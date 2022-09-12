d433a10e53c9a8873ecce82b45b825076e102480
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2022-09-12 21:30:55 +0530

    compareValues function add

---
+++ lucky.js
@@ -0,0 +1,38 @@
+// all variables
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const check = document.querySelector("#check")
+const answer = document.querySelector("#answer")
+
+
+function compareValues(sum, luckyNumber) 
+{
+if (sum%luckyNumber === 0) {
+answer.innerHTML="Your birthday is lucky 🍕"
+}else{
+answer.innerHTML="No it's not lucky 😿"
+}
+}
+
+
+function checkBirthDateIsLucky() {
+  const dob = dateOfBirth.value;
+  const sum = calculateSum(dob);
+  if(sum && dob)
+  compareValues(sum, luckyNumber.value)
+  else
+  answer.innerHTML="Please add both value 😾"
+}
+
+
+function calculateSum(dob) {
+  dob = dob.replaceAll("-","");
+  let sum = 0;
+  for(let i=0;i<dob.length; i++){
+    sum = sum + Number(dob.charAt(i));
+  }
+  return sum;
+}
+
+check.addEventListener("click",
+checkBirthDateIsLucky)
